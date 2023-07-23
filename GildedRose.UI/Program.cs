95cdbc5a59d0ee742b799c9aebd0d0326ec16e33
citrusian <115960900+citrusian@users.noreply.github.com>
2023-07-23 11:24:27 +0700

    Revert extract basic function

--- GildedRose.UI/Program.cs
+++ GildedRose.UI/Program.cs
@@ -59,117 +59,52 @@ namespace GildedRose.Console
             System.Console.ReadKey();
         }
 
-        // Menggunakan Extract Method untuk beberapa basic function
-
-        // Alt Function, called using IncreaseQuality(item, 1);
-        //private void IncreaseQuality(Item item, int amount)
-        //{
-        //    System.Console.WriteLine($"Item '{item.Name}' Original Q = {item.Quality}"); // Debug
-        //    item.Quality += amount;
-        //    System.Console.WriteLine($"Item '{item.Name}' Updated Q = {item.Quality}\n"); // Debug
-        //}
-
-        private void SetQualityZero(Item item)
-        {
-            System.Console.WriteLine($"Item '{item.Name}' Original Q = {item.Quality}"); // Debug
-            item.Quality = item.Quality - item.Quality;
-            System.Console.WriteLine($"Item '{item.Name}' Updated Q = {item.Quality}\n"); // Debug
-        }
-        private void IncreaseQualityByOne(Item item)
-        {
-            System.Console.WriteLine($"Item '{item.Name}' Original Q = {item.Quality}"); // Debug
-            item.Quality++;
-            System.Console.WriteLine($"Item '{item.Name}' Updated Q = {item.Quality}\n"); // Debug
-        }
-        private void DecreaseQualityByOne(Item item)
-        {
-            System.Console.WriteLine($"Item '{item.Name}' Original Q = {item.Quality}"); // Debug
-            item.Quality--;
-            System.Console.WriteLine($"Item '{item.Name}' Updated Q = {item.Quality}\n"); // Debug
-        }
-        private void IncreaseSellinByOne(Item item)
-        {
-            System.Console.WriteLine($"Item '{item.Name}' Original S = {item.SellIn}"); // Debug
-            item.SellIn++;
-            System.Console.WriteLine($"Item '{item.Name}' Updated S = {item.SellIn}\n"); // Debug
-        }
-        private void DecreaseSellinByOne(Item item)
-        {
-            System.Console.WriteLine($"Item '{item.Name}' Original S = {item.SellIn}"); // Debug
-            item.SellIn--;
-            System.Console.WriteLine($"Item '{item.Name}' Updated S = {item.SellIn}\n"); // Debug
-        }
-
 
         public void UpdateQuality()
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                //if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                //{
-                //    if (Items[i].Quality > 0)
-                //    {
-                //        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                //        {
-                //            DecreaseQualityByOne(Items[i]);
-                //        }
-                //    }
-                //}
-                //else
-                //{
-                //    if (Items[i].Quality < 50)
-                //    {
-                //        IncreaseQualityByOne(Items[i]);
-
-                //        // Combine Duplicate Logic (Quality < 50)
-                //        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[i].Quality < 50)
-                //        {
-                //            if (Items[i].SellIn < 11)
-                //            {
-                //                IncreaseQualityByOne(Items[i]);
-                //            }
-
-                //            if (Items[i].SellIn < 6)
-                //            {
-                //                IncreaseQualityByOne(Items[i]);
-                //            }
-                //        }
-                //    }
-                //}
-
-
-
                 // Invert condition, then use guard clause to reduce nested if
                 if (Items[i].Name == "Aged Brie" || Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (Items[i].Quality < 50)
                     {
-                        IncreaseQualityByOne(Items[i]);
+                        System.Console.WriteLine($"Item '{Items[i].Name}' Original Q = {Items[i].Quality}"); // Debug
+                        Items[i].Quality++;
+                        System.Console.WriteLine($"Item '{Items[i].Name}' Updated Q = {Items[i].Quality}\n"); // Debug
 
                         // Cannot be combined to retain the logic, looking at original code
-                        // "IncreaseQualityByOne(Items[i]);" must be called twice if "Items[i].SellIn < 6"
+                        // "Items[i].Quality++; " must be called twice if "Items[i].SellIn < 6"
                         // remove unused / duplicate condition "Quality < 50", already evaluted at outer block
                         if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[i].SellIn < 11)
                         {
-                            IncreaseQualityByOne(Items[i]);
+                            System.Console.WriteLine($"Item '{Items[i].Name}' Original Q = {Items[i].Quality}"); // Debug
+                            Items[i].Quality++;
+                            System.Console.WriteLine($"Item '{Items[i].Name}' Updated Q = {Items[i].Quality}\n"); // Debug
                         }
 
                         if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[i].SellIn < 6)
                         {
-                            IncreaseQualityByOne(Items[i]);
+                            System.Console.WriteLine($"Item '{Items[i].Name}' Original Q = {Items[i].Quality}"); // Debug
+                            Items[i].Quality++;
+                            System.Console.WriteLine($"Item '{Items[i].Name}' Updated Q = {Items[i].Quality}\n"); // Debug
                         }
                     }
                 }
                 else if (Items[i].Quality > 0 && Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    DecreaseQualityByOne(Items[i]);
+                    System.Console.WriteLine($"Item '{Items[i].Name}' Original Q = {Items[i].Quality}"); // Debug
+                    Items[i].Quality--;
+                    System.Console.WriteLine($"Item '{Items[i].Name}' Updated Q = {Items[i].Quality}\n"); // Debug
                 }
 
 
                 // Cannot be combined to retain the logic
                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    DecreaseSellinByOne(Items[i]);
+                    System.Console.WriteLine($"Item '{Items[i].Name}' Original Q = {Items[i].SellIn}"); // Debug
+                    Items[i].SellIn--;
+                    System.Console.WriteLine($"Item '{Items[i].Name}' Updated Q = {Items[i].SellIn}\n"); // Debug
                 }
 
 
@@ -178,15 +113,21 @@ namespace GildedRose.Console
                     // Invert condition, then use guard clause to reduce nested if
                     if (Items[i].Name == "Aged Brie" && Items[i].Quality < 50)
                     {
-                        IncreaseQualityByOne(Items[i]);
+                        System.Console.WriteLine($"Item '{Items[i].Name}' Original Q = {Items[i].Quality}"); // Debug
+                        Items[i].Quality++;
+                        System.Console.WriteLine($"Item '{Items[i].Name}' Updated Q = {Items[i].Quality}\n"); // Debug
                     }
                     else if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        SetQualityZero(Items[i]);
+                        System.Console.WriteLine($"Item '{Items[i].Name}' Original Q = {Items[i].Quality}"); // Debug
+                        Items[i].Quality=0;
+                        System.Console.WriteLine($"Item '{Items[i].Name}' Updated Q = {Items[i].Quality}\n"); // Debug
                     }
                     else if (Items[i].Quality > 0 && Items[i].Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        DecreaseQualityByOne(Items[i]);
+                        System.Console.WriteLine($"Item '{Items[i].Name}' Original Q = {Items[i].Quality}"); // Debug
+                        Items[i].Quality--;
+                        System.Console.WriteLine($"Item '{Items[i].Name}' Updated Q = {Items[i].Quality}\n"); // Debug
                     }
                 }
             }
