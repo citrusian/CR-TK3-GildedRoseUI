a993e72bbc2c17c72dd21602269631df7d369248
citrusian <115960900+citrusian@users.noreply.github.com>
2023-07-23 11:25:14 +0700

    cleanup

--- GildedRose.UI/Program.cs
+++ GildedRose.UI/Program.cs
@@ -69,42 +69,32 @@ namespace GildedRose.Console
                 {
                     if (Items[i].Quality < 50)
                     {
-                        System.Console.WriteLine($"Item '{Items[i].Name}' Original Q = {Items[i].Quality}"); // Debug
                         Items[i].Quality++;
-                        System.Console.WriteLine($"Item '{Items[i].Name}' Updated Q = {Items[i].Quality}\n"); // Debug
 
                         // Cannot be combined to retain the logic, looking at original code
                         // "Items[i].Quality++; " must be called twice if "Items[i].SellIn < 6"
                         // remove unused / duplicate condition "Quality < 50", already evaluted at outer block
                         if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[i].SellIn < 11)
                         {
-                            System.Console.WriteLine($"Item '{Items[i].Name}' Original Q = {Items[i].Quality}"); // Debug
                             Items[i].Quality++;
-                            System.Console.WriteLine($"Item '{Items[i].Name}' Updated Q = {Items[i].Quality}\n"); // Debug
                         }
 
                         if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[i].SellIn < 6)
                         {
-                            System.Console.WriteLine($"Item '{Items[i].Name}' Original Q = {Items[i].Quality}"); // Debug
                             Items[i].Quality++;
-                            System.Console.WriteLine($"Item '{Items[i].Name}' Updated Q = {Items[i].Quality}\n"); // Debug
                         }
                     }
                 }
                 else if (Items[i].Quality > 0 && Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    System.Console.WriteLine($"Item '{Items[i].Name}' Original Q = {Items[i].Quality}"); // Debug
                     Items[i].Quality--;
-                    System.Console.WriteLine($"Item '{Items[i].Name}' Updated Q = {Items[i].Quality}\n"); // Debug
                 }
 
 
                 // Cannot be combined to retain the logic
                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    System.Console.WriteLine($"Item '{Items[i].Name}' Original Q = {Items[i].SellIn}"); // Debug
                     Items[i].SellIn--;
-                    System.Console.WriteLine($"Item '{Items[i].Name}' Updated Q = {Items[i].SellIn}\n"); // Debug
                 }
 
 
@@ -113,21 +103,15 @@ namespace GildedRose.Console
                     // Invert condition, then use guard clause to reduce nested if
                     if (Items[i].Name == "Aged Brie" && Items[i].Quality < 50)
                     {
-                        System.Console.WriteLine($"Item '{Items[i].Name}' Original Q = {Items[i].Quality}"); // Debug
                         Items[i].Quality++;
-                        System.Console.WriteLine($"Item '{Items[i].Name}' Updated Q = {Items[i].Quality}\n"); // Debug
                     }
                     else if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        System.Console.WriteLine($"Item '{Items[i].Name}' Original Q = {Items[i].Quality}"); // Debug
                         Items[i].Quality=0;
-                        System.Console.WriteLine($"Item '{Items[i].Name}' Updated Q = {Items[i].Quality}\n"); // Debug
                     }
                     else if (Items[i].Quality > 0 && Items[i].Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        System.Console.WriteLine($"Item '{Items[i].Name}' Original Q = {Items[i].Quality}"); // Debug
                         Items[i].Quality--;
-                        System.Console.WriteLine($"Item '{Items[i].Name}' Updated Q = {Items[i].Quality}\n"); // Debug
                     }
                 }
             }
